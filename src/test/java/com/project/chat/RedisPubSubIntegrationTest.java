f375cdb01a9a2fb4a2b4cd7e670a9d6e8aadf8ba
Koo-EunSung <ku903@naver.com>
2025-12-12 00:20:52 +0900

    Test: Redis pub/sub 통합 테스트 추가

---
+++ src/test/java/com/project/chat/RedisPubSubIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.project.chat;
+
+import com.project.chat.dto.ChatMessageDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import tools.jackson.databind.ObjectMapper;
+
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(TestRedisConfig.class)
+@SpringBootTest
+@Testcontainers
+public class RedisPubSubIntegrationTest {
+    private static final int PORT = 6379;
+
+    @Autowired
+    private RedisPublisher redisPublisher;
+
+    @Autowired
+    private ChannelTopic channelTopic;
+
+    @MockitoBean
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("Redis로 메시지를 발행하면 Subscriber가 수신해서 STOMP로 전송한다.")
+    @Test
+    void redisPubSub() throws Exception {
+        final String USER = "user";
+        final String CONTENT = "Test";
+        ChatMessageDTO message = new ChatMessageDTO(USER, CONTENT);
+
+        redisPublisher.publish(channelTopic, message);
+
+        verify(messagingTemplate, timeout(5000).times(1))
+                .convertAndSend(eq("/topic/chat"), any(ChatMessageDTO.class));
+    }
+}
