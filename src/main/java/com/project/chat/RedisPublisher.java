f7567fa18a09d287cfda18a5cdb8f043478a2379
Koo-EunSung <ku903@naver.com>
2026-01-09 16:48:09 +0900

    Refactor: redis pub/sub에서 request, event, response 사용

--- src/main/java/com/project/chat/RedisPublisher.java
+++ src/main/java/com/project/chat/RedisPublisher.java
@@ -1,17 +1,28 @@
 package com.project.chat;
 
-import com.project.chat.dto.ChatMessageDTO;
+import com.github.f4b6a3.tsid.TsidCreator;
+import com.project.chat.dto.ChatMessageSendRequest;
+import com.project.chat.event.ChatMessageEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public void publish(ChannelTopic topic, ChatMessageDTO message) {
-        redisTemplate.convertAndSend(topic.getTopic(), message);
+    public void publish(String topic, ChatMessageSendRequest message) {
+        ChatMessageEvent event = new ChatMessageEvent(
+                TsidCreator.getTsid(),
+                message.getRoomId(),
+                message.getSender(),
+                message.getContent(),
+                Instant.now()
+        );
+
+        redisTemplate.convertAndSend(topic, event);
     }
 }
--- src/main/java/com/project/chat/RedisSubscriber.java
+++ src/main/java/com/project/chat/RedisSubscriber.java
@@ -1,8 +1,10 @@
 package com.project.chat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.chat.dto.ChatMessageDTO;
+import com.project.chat.dto.ChatMessageResponse;
+import com.project.chat.event.ChatMessageEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,17 @@ public class RedisSubscriber {
     private final ObjectMapper objectMapper;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    public void onMessage(String publishMessage) {
+    public void onMessage(Message message, byte[] pattern) {
         try {
-            ChatMessageDTO chatMessageDTO = objectMapper.readValue(publishMessage, ChatMessageDTO.class);
-            messagingTemplate.convertAndSend("/topic/chat", chatMessageDTO);
+            ChatMessageEvent event = objectMapper.readValue(message.getBody(), ChatMessageEvent.class);
+            ChatMessageResponse response = new ChatMessageResponse(
+                    event.getId(),
+                    event.getRoomId(),
+                    event.getSender(),
+                    event.getContent(),
+                    event.getSentAt()
+            );
+            messagingTemplate.convertAndSend("/topic/chat", response);
         } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
