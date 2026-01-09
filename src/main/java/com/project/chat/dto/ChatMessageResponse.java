fdeb85377b604c7f822912aba2705cd9458a58ce
Koo-EunSung <ku903@naver.com>
2026-01-09 15:36:20 +0900

    refactor: ChatMessageResponse.id 타입을 tsid로 변경

--- src/main/java/com/project/chat/dto/ChatMessageResponse.java
+++ src/main/java/com/project/chat/dto/ChatMessageResponse.java
@@ -1,14 +1,17 @@
 package com.project.chat.dto;
 
+import com.github.f4b6a3.tsid.Tsid;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.Instant;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ChatMessageResponse {
-    private String id;
+    private Tsid id;
     private String roomId;
     private String sender;
     private String content;
