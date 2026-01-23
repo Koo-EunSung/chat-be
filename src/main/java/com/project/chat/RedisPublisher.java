f73c91f46008577813bba8d83d566d86eda7ee54
Koo-EunSung <ku903@naver.com>
2026-01-23 22:45:08 +0900

    Feat: 메시지 발행 시 ChatMessagePublishedEvent 발행

--- src/main/java/com/project/chat/RedisPublisher.java
+++ src/main/java/com/project/chat/RedisPublisher.java
@@ -2,7 +2,9 @@ package com.project.chat;
 
 import com.project.chat.dto.ChatMessagePayload;
 import com.project.chat.dto.ChatMessageSendRequest;
+import com.project.chat.event.ChatMessagePublishedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RedisPublisher {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ApplicationEventPublisher eventPublisher;
 
     public void publish(String roomId, ChatMessageSendRequest message) {
         ChatMessagePayload event = ChatMessagePayload.from(message);
 
         redisTemplate.convertAndSend("chat/room/" + roomId, event);
+        eventPublisher.publishEvent(new ChatMessagePublishedEvent(this, event));
     }
 }
