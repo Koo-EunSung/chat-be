1e6dcdca9b41a1f13ed03ed82b345e9231689919
Koo-EunSung <ku903@naver.com>
2026-05-22 17:07:37 +0900

    Remove: Redis 관련 코드 제거

--- src/main/java/com/project/chat/controller/ChatController.java
+++ src/main/java/com/project/chat/controller/ChatController.java
@@ -1,19 +1,14 @@
 package com.project.chat.controller;
 
-import com.project.chat.RedisPublisher;
 import com.project.chat.dto.ChatMessageSendRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequiredArgsConstructor
 public class ChatController {
-    private final RedisPublisher redisPublisher;
 
     @MessageMapping("/chat/room/{roomId}")
     public void message(@DestinationVariable String roomId, ChatMessageSendRequest message) {
-        redisPublisher.publish(roomId, message);
     }
 }
